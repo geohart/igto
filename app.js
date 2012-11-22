33d8a9602f768f7fc52a1b71f6ead2fb5b9374a1
geohart <george@sidelime.com>
2012-11-22 11:53:45 -0800

    refactor out path to app.js and update invite.js to reflect this

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 2001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('path', 'localhost:' + app.get('port'));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
